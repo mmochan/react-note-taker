0615ed60c22615e95f4c1357ce469c8254b797f3
Mike Mochan <mmochan@mac.com>
2016-11-30 18:50:51 +1000

    add avatar_url

--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -5,7 +5,7 @@ class UserProfile extends React.Component {
   render(){
     return (
       <div>
-     {/*   {this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>} */}
+        {this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
         {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
         {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
         {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
